b208b743ce7e684fca9e0b52b64d3d699353000d
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-08 18:38:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(number)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+   i = number
+  while i > 0 
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+    i -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
